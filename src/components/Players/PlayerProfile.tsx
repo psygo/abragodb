d92fd8a450b50f485a442ef38ba9e7e9e7e1749b
Philippe Fanaro <philippefanaro@gmail.com>
2024-05-09 15:54:25 -0300

    more profile stuff

--- src/components/Players/PlayerProfile.tsx
+++ src/components/Players/PlayerProfile.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image"
+
 import e, { type $infer } from "@@/dbschema/edgeql-js"
 
 import { type Username } from "@types"
-import Image from "next/image"
+
+import { type GoUsers, goStrength } from "@validation"
+import { localDateToDate } from "../../utils/datetime"
 
 export function getPlayerQuery(username: Username) {
   return e.select(e.Player, (player) => ({
@@ -32,9 +36,42 @@ export function PlayerProfile({
       <h2 className="text-gray-400">Nenhuma Informacao</h2>
     )
 
+  function getMaxStrength() {
+    const goUsers = player?.profile?.go_users as GoUsers
+    if (!goUsers) return ""
+
+    const strengths = Object.values({ ...goUsers })
+    if (strengths.length === 0) return ""
+
+    const maxStrength = strengths
+      .map((u) => {
+        return {
+          ...goStrength.find(
+            (gs) => gs.kyu_dan === u.strength,
+          )!,
+          server: u.server,
+        }
+      })
+      .reduce((p, v) => (p >= v ? p : v))
+
+    return `${maxStrength.kyu_dan} ${maxStrength.server}`
+  }
+
+  function getAge() {
+    const dateOfBirth = player?.profile?.date_of_birth
+    if (!dateOfBirth) return ""
+    const d = localDateToDate(dateOfBirth)
+
+    const now = new Date()
+    const ageDifMs = now.getTime() - d.getTime()
+    const ageDate = new Date(ageDifMs)
+    const age = Math.abs(ageDate.getUTCFullYear() - 1970)
+    return `${age} anos`
+  }
+
   return (
     <section className="mt-1">
-      <div className="flex gap-2">
+      <div className="flex gap-2 items-center">
         <PlayerAvatar
           imageUrl={player.image_url}
           alt={player.username}
@@ -42,8 +79,12 @@ export function PlayerProfile({
         <h2 className="mt-6 flex gap-2">
           <span>{player.profile?.first_name}</span>
           <span>{player.profile?.last_name}</span>
+          <span className="text-gray-600">
+            {getMaxStrength()}
+          </span>
         </h2>
       </div>
+      <p>{getAge()}</p>
       <p>{player.profile?.description}</p>
       <p>{player.profile?.public_email}</p>
       <p>{player.profile?.date_of_birth?.toString()}</p>
@@ -71,6 +112,7 @@ export function PlayerAvatar({
 
   return (
     <Image
+      className="my-4"
       src={imageSrc}
       width={50}
       height={50}
