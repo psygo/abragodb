d29b4d2e26a35edbe979447e5a7bb619db841eba
Philippe Fanaro <philippefanaro@gmail.com>
2024-05-10 14:19:20 -0300

    better about page

--- src/app/(routes)/sobre/page.tsx
+++ src/app/(routes)/sobre/page.tsx
@@ -3,12 +3,14 @@ export default function AboutPage() {
     <article className="prose dark:prose-invert">
       <section>
         <h2 className="mt-6">Sobre o Projeto</h2>
+
         <p>
           O ABRAGO DB é um banco de dados interativo para
           jogadores brasileiros. Como uma segunda etapa do
           projeto, há intenções de se adicionar partidas e
           rankings.
         </p>
+
         <p>
           O site é todo desenvolvido em código aberto, e
           você pode contribuir com código ou sugestões
@@ -18,6 +20,18 @@ export default function AboutPage() {
           </a>
           .
         </p>
+
+        <p>
+          Algumas das funcionalidades que estão planejadas:
+        </p>
+
+        <ul>
+          <li>Estatísticas da População de Jogadores</li>
+          <li>Mapa Interativo com os Jogadores</li>
+          <li>Internacionalização</li>
+          <li>Partidas</li>
+          <li>Torneios</li>
+        </ul>
       </section>
     </article>
   )
