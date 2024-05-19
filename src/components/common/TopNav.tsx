3f03d191a3375d60d5413acf6308fefe4a380c8e
Philippe Fanaro <philippefanaro@gmail.com>
2024-05-19 16:11:26 -0300

    much better top nav

--- src/components/common/TopNav.tsx
+++ src/components/common/TopNav.tsx
@@ -55,42 +55,91 @@ function LeftNav() {
 }
 
 function RightNav() {
+  const { cycleTheme } = useTheme()
+  const { user } = useClerkUser()
+
   return (
-    <section className="flex items-center md:gap-1">
-      <PageNavs />
-      <ThemeButton />
-      <EditProfileButton />
-      <ClerkSignIn />
+    <section>
+      <Menubar className="bg-transparent px-0 w-max md:hidden">
+        <MenubarMenu>
+          <MenubarTrigger className="cursor-pointer">
+            <Menu />
+          </MenubarTrigger>
+          <MenubarContent className="flex flex-col gap-1 py-2 md:hidden">
+            <MenubarItem className="justify-end">
+              <Link
+                href="/estatisticas"
+                className="text-[1.15rem]"
+              >
+                Estatísticas
+              </Link>
+            </MenubarItem>
+            <MenubarItem className="justify-end">
+              <Link
+                href="/sobre"
+                className="text-[1.15rem]"
+              >
+                Sobre
+              </Link>
+            </MenubarItem>
+            <MenubarItem className="justify-end">
+              <Button
+                variant="ghost"
+                size="icon"
+                className="w-max h-max"
+                onClick={async () => await cycleTheme()}
+              >
+                <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute mr-2 h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <p className="sr-only">Toggle theme</p>
+              </Button>
+            </MenubarItem>
+            <MenubarItem className="justify-end">
+              <SignedIn>
+                <Link
+                  className="flex items-center"
+                  href={`/jogadores/${user?.username}`}
+                >
+                  <Button
+                    variant="ghost"
+                    className="p-0 mr-1"
+                  >
+                    <Pencil className="w-[22px] h-22px" />
+                  </Button>
+                </Link>
+              </SignedIn>
+            </MenubarItem>
+            <MenubarItem className="justify-end">
+              <SignedOut>
+                <SignInButton>Entrar</SignInButton>
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
+
+      <div className="hidden md:flex items-center gap-1">
+        <PageNavs />
+        <ThemeButton />
+        <EditProfileButton />
+        <ClerkSignIn />
+      </div>
     </section>
   )
 }
 
 function PageNavs() {
   return (
-    <>
-      <div className="mr-4 hidden md:flex gap-2">
-        <PageNavLink
-          href="/estatisticas"
-          label="Estatísticas"
-        />
-        <PageNavLink href="/sobre" label="Sobre" />
-      </div>
-      <Menubar className="bg-transparent px-0 w-max md:hidden">
-        <MenubarMenu>
-          <MenubarTrigger>
-            <Menu />
-          </MenubarTrigger>
-          <MenubarContent>
-            <MenubarItem>
-              <Link href="/estatisticas">Estatísticas</Link>
-            </MenubarItem>
-            <MenubarItem>
-              <Link href="/sobre">Sobre</Link>
-            </MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-      </Menubar>
-    </>
+    <div className="mr-4 hidden md:flex gap-2">
+      <PageNavLink
+        href="/estatisticas"
+        label="Estatísticas"
+      />
+      <PageNavLink href="/sobre" label="Sobre" />
+    </div>
   )
 }
 
@@ -120,27 +169,10 @@ function ThemeButton() {
       size="icon"
       onClick={async () => await cycleTheme()}
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      <span className="sr-only">Toggle theme</span>
+      <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <p className="sr-only">Toggle theme</p>
     </Button>
-  )
-}
-
-function ClerkSignIn() {
-  return (
-    <div className="ml-3 flex items-center">
-      <SignedOut>
-        <SignInButton>
-          <Button size="sm" variant="outline">
-            Entrar
-          </Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
   )
 }
 
@@ -158,5 +190,22 @@ function EditProfileButton() {
         </Button>
       </Link>
     </SignedIn>
+  )
+}
+
+function ClerkSignIn() {
+  return (
+    <div className="ml-3 flex items-center">
+      <SignedOut>
+        <SignInButton>
+          <Button size="sm" variant="outline">
+            Entrar
+          </Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
   )
 }
